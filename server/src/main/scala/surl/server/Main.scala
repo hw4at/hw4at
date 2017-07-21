050af47a7f897a057ca62f93f13720cf3010a433
Moshe Jacobson <moshe.j@matomy.com>
2017-07-21 17:58:32 +0300

    trying scala project

---
+++ server/src/main/scala/surl/server/Main.scala
@@ -0,0 +1,11 @@
+package surl.server
+
+object Main {
+  def main(args: Array[String]) {
+    println("I'm up!")
+    while (true) {
+      println("Running ...")
+      Thread.sleep(3000)
+    }
+  }
+}
